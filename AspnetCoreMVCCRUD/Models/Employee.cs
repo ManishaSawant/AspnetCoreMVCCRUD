a3e20adf77373837461d62135ffba4a710443389
Manisha Sawant <mnshpowar@gmail.com>
2019-09-26 12:30:29 +0530

    Initial commit

---
+++ AspnetCoreMVCCRUD/Models/Employee.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel;
+
+namespace AspnetCoreMVCCRUD.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int EmployeeId { get; set; }
+        [Required(ErrorMessage ="This field is required")]
+        [Column(TypeName = "varchar(250)")]
+
+        [DisplayName("Full Name")]
+        public string FullName { get; set; }
+       
+        [Column(TypeName = "varchar(10)")]
+        [DisplayName("Employee Code")]
+        public string EmepCode { get; set; }
+        [Column(TypeName = "varchar(100)")]
+        [DisplayName("Position")]
+        public string Position { get; set; }
+        [Column(TypeName = "varchar(100)")]
+        [DisplayName("Office Location")]
+        public string OfficeLocation { get; set; }
+
+    }
+}
